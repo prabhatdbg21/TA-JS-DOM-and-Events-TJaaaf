010d19bef3aa7c04fe5e109f615590523493102d
Prabhat <prabhatdbg21@gmail.com>
2022-06-27 11:30:00 +0530

    block-BJaaew

--- block-BJaaew/code/script.js
+++ block-BJaaew/code/script.js
@@ -1,170 +1,63 @@
-let bigDiv = document.querySelector ('.all-box');
-let newbox = document.querySelector ('.newbox')
-got.houses.forEach((house) => {
-    house.people.forEach(peoples => {
-            let article = document.createElement('article');
-            article.classList.add('box')
-        
-            let div = document.createElement('div');
-            div.classList.add('flex-justify-content')
-            let img = document.createElement('img');
-            img.src = peoples.image;
-            div.append (img)
-        
-            let h2 = document.createElement('h2');
-            h2.innerText = peoples.name ;
-            
-        
-            let p = document.createElement('p');
-            p.innerText = peoples.description;
-            
-            let a = document.createElement('a');
-            a.innerText = `KNOW MORE!` ;
-            a.href = peoples.wikiLink;
-        
-            article.append (div , h2 ,p , a);
-        
-            bigDiv.append (article)
+let root = document.querySelector ('.cards');
+let rootTags = document.querySelector ('.tags');
+let search = document.querySelector ('.search')
 
-    });
-});
+let allPeople = got.houses.reduce ((acc , cv) =>{
+    acc = acc.concat (cv.people) ;
+    return acc ;
+}, []) ;
 
-function search (event){
-    bigDiv.classList.add ('displayNone')
-    console.dir (event.target.value)
-    got.houses.forEach((house) => {
-        house.people.forEach(peoples => {
-                let article = document.createElement('article');
-                article.classList.add('box')
-            
-                let div = document.createElement('div');
-                div.classList.add('flex-justify-content')
-                let img = document.createElement('img');
-                img.src = peoples.image;
-                div.append (img)
-            
-                let h2 = document.createElement('h2');
-                h2.innerText = peoples.name ;
-                
-            
-                let p = document.createElement('p');
-                p.innerText = peoples.description;
-                
-                let a = document.createElement('a');
-                a.innerText = `KNOW MORE!` ;
-                a.href = peoples.wikiLink;
-            
-                article.append (div , h2 ,p , a);
-            
-                newbox.append (article)
-    
-        });
+let allTags = got.houses.map ((house) => house.name); 
+
+let activeHouse = "" ;
+
+function createCards ( data = []) {
+    root.innerHTML = "" ;
+    data.forEach((people) => {
+        let li = document.createElement('li'); 
+        let img = document.createElement('img');
+        img.alt = people.name ;
+        img.src = people.image ;
+        let h2 = document.createElement('h2');
+        h2.innerText = people.name ;
+        let p = document.createElement('p');
+        p.innerText = people.description;
+        let button = document.createElement('button');
+        button.innerText = `KNOW MORE!` ;
+        
+        li.append (img , h2 ,p , button);
+        
+        root.append (li)
     });
 }
 
-let input = document.querySelector ('input')
-input.addEventListener ("keyup" , search)
+function createTagsUi(tags = []) {
+    rootTags.innerHTML = "" ;
+    tags.forEach((tag) => {
+        let li = document.createElement('li'); 
+        li.innerText = tag ;
 
+        if( activeHouse === tag){
+            li.classList.add("active");
+        }
 
-let starks = document.querySelector ('.starks') 
-let lannisters = document.querySelector ('.lannisters')
-let baratheons = document.querySelector ('.baratheons')
-let targaryens = document.querySelector ('.targaryens')
-let greyjoys = document.querySelector ('.greyjoys')
-let tyrells = document.querySelector ('.tyrells')
-let tullys = document.querySelector ('.tullys')
-let redwyne = document.querySelector ('.redwyne')
-let freys = document.querySelector ('.freys')
-let arryns = document.querySelector ('.arryns')
-let dothrakis = document.querySelector ('.dothrakis')
-
-let getHouses = got.houses 
-
-
-
-
-function people(peopl) {
-    bigDiv.classList.add ('displayNone')
-    let article = document.createElement('article');
-    article.classList.add('box')
-
-    let div = document.createElement('div');
-    div.classList.add('flex-justify-content')
-    let img = document.createElement('img');
-    img.src = peopl.image;
-    div.append (img)
-
-    let h2 = document.createElement('h2');
-    h2.innerText = peopl.name ;
-    
-
-    let p = document.createElement('p');
-    p.innerText = peopl.description;
-    
-    let a = document.createElement('a');
-    a.innerText = `KNOW MORE!` ;
-    a.href = peopl.wikiLink;
-
-    article.append (div , h2 ,p , a);
-
-    newbox.append (article)
+        li.addEventListener("click" , () => {
+            activeHouse = tag ;
+            let peopleOfTheHouse = got.houses.find ((house) => house.name === tag).people || [];
+            createCards (peopleOfTheHouse) ;
+            createTagsUi (allTags) ;
+        })
+        rootTags.append (li)
+    });
 }
 
-
-function handleClick (event) { 
-    got.houses[0].people.forEach(peopl => {  people(peopl) });
+function handleSearch (event) {
+    let serchText = event.target.value ;
+    let filteredPeople = allPeople.filter ( (people) =>  people.name.toLowerCase().includes( serchText.toLowerCase())  )
+    createCards (filteredPeople) ;
 }
 
-function handleClicka (event) { 
-    console.log (event.target)
-    got.houses[1].people.forEach(peopl => {  people(peopl) });
-}
+search.addEventListener ("input" , handleSearch) ;
 
-function handleClick2 (event) { 
-    got.houses[2].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick3 (event) { 
-    got.houses[3].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick4 (event) { 
-    got.houses[4].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick5 (event) { 
-    got.houses[5].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick6 (event) { 
-    got.houses[6].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick7 (event) { 
-    got.houses[7].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick8 (event) { 
-    got.houses[8].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick9 (event) { 
-    got.houses[9].people.forEach(peopl => {  people(peopl) });
-}
-
-function handleClick10 (event) { 
-    got.houses[10].people.forEach(peopl => {  people(peopl) });
-}
-
-starks.addEventListener ("click" , handleClick )
-lannisters.addEventListener ("click" , handleClicka)
-baratheons.addEventListener ("click" , handleClick2 )
-targaryens.addEventListener ("click" , handleClick3)
-greyjoys.addEventListener ("click" , handleClick4 )
-tyrells.addEventListener ("click" , handleClick5 )
-tullys.addEventListener ("click" , handleClick6 )
-redwyne.addEventListener ("click" , handleClick7)
-freys.addEventListener ("click" , handleClick8 )
-arryns.addEventListener ("click" , handleClick9 )
-dothrakis.addEventListener ("click" , handleClick10)
-
+createCards (allPeople) ;
+createTagsUi (allTags) ;
